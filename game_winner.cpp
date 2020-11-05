3aaac6a0c2236349339679a8f874c1dd789ed9f3
Naveen Chandra <naveenchandra259@gmail.com>
2020-11-05 15:01:11 +0530

    Update game_winner.cpp

--- game_winner.cpp
+++ game_winner.cpp
@@ -6,7 +6,7 @@ int main() {
 	string s = "wwwbbbbwww";
 	int size = s.length();
 	
-	priority_queue<int> w, b;
+	int w=0, b=0;
 	
 	int left = 0, ctr = 0;
 	
@@ -22,50 +22,15 @@ int main() {
 	        }
 	        if(ctr-1 > 0)
 	        {
-	            if(s[left] == 'w') w.push(ctr-1);
-	            else b.push(ctr-1);
+	            if(s[left] == 'w') w+=ctr-1;
+	            else b+=ctr-1;
 	        }
 	    }
 	    left++;
 	}
 	
-	if(w.empty()) cout<<"wendy";
-	if(b.empty()) cout<<"bob";
-	
-	int curr = 1, temp = 0;
-	//1 for wendy
-	//0 for bob
-	
-	while(!w.empty() && !b.empty())
-	{
-	    if(curr == 1)
-	    {
-	        temp = w.top()-1;
-	        w.pop();
-	        if(temp>0) w.push(temp);
-	        curr = 0;
-	    }
-	    
-	    else
-	    {
-	        temp = b.top()-1;
-	        b.pop();
-	        if(temp>0) b.push(temp);
-	        curr = 1;
-	    }
-	}
-	
-	if(curr == 1)
-	{
-	    if(w.size()>0) cout<<"bob";
-	    else cout<<"wendy";
-	}
-	
-	else
-	{
-	    if(b.size()>0) cout<<"wendy";
-	    else cout<<"bob";
-	}
+	if(w <= b) cout<<"wendy";
+	else cout<<"bob";
 	
 	return 0;
 }
